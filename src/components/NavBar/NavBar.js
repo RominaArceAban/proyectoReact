b01df56cb4b7766831aeac02f84c5a37342d1d5c
Romina Arce Aban <rarceaban@outlook.com>
2022-03-28 02:44:14 -0300

    se agregaron clases al NavBar.css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,26 +4,14 @@ const NavBar = () => {
     return(
         <nav className="NavBar">
             <img></img>
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Remeras</a></li>
-                <li><a href="#">Buzos</a></li>
-                <li><a href="#">Camisas</a></li>
+            <button className="boton">Inicio</button>
+            <ul className="NavBarList">
+                <li><a className="NavBarList" href="#">Remeras</a></li>
+                <li><a className="NavBarList" href="#">Buzos</a></li>
+                <li><a className="NavBarList" href="#">Camisas</a></li>
             </ul>
-            <button>Carrito</button>
+            <button className="boton">Login</button>
         </nav>
-        /*<header class="header">
-        <img class="header__logo" src="./assets/logo.png"  alt="Logo">
-        <nav class="header__nav">
-            <ul class="header__list">
-                <li class="header__item"><a class="header__item--links" href="#">Inicio</a></li>
-                <li class="header__item"><a class="header__item--links" href="./secciones/Remeras.html">Remeras</a></li>
-                <li class="header__item"><a class="header__item--links" href="./secciones/Buzos.html"> Buzos</a></li>
-                <li class="header__item"><a class="header__item--links" href="./secciones/Talles.html">Talles</a></li>
-                <li class="header__item"><a class="header__item--links" href="./secciones/Contactanos.html">Contacto</a></li>
-            </ul>
-        </nav>
-        </header>*/
     )
 }
 
